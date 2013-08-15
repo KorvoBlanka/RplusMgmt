831da6406c0ad9b1ba0a17d5e9122fe249c1520f
Alexander Orlenko <zxteam@gmail.com>
2013-08-15 13:07:09 +1100

    RplusMgmt::Landmark module

---
+++ lib/RplusMgmt/Controller/API/Landmark.pm
@@ -0,0 +1,117 @@
+package RplusMgmt::Controller::API::Landmark;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Rplus::Model::Landmark;
+use Rplus::Model::Landmark::Manager;
+
+use Mojo::Util qw(trim);
+
+use JSON;
+
+sub auth {
+    my $self = shift;
+
+    my $user_role = $self->session->{'user'}->{'role'};
+    if ($user_role && $self->config->{'roles'}->{$user_role}->{'configuration'}->{'landmarks'}) {
+        return 1;
+    }
+
+    $self->render_not_found;
+    return undef;
+}
+
+sub list {
+    my $self = shift;
+
+    my $type = $self->param('type');
+
+    my $res = {
+        count => 0,
+        list => [],
+    };
+
+    my @fields = qw(id name);
+    my $landmark_iter = Rplus::Model::Landmark::Manager->get_objects_iterator(select => [@fields], query => [type => $type, delete_date => undef], sort_by => 'name');
+    while (my $landmark = $landmark_iter->next) {
+        push @{$res->{'list'}}, {map { $_ => $landmark->$_ } @fields};
+        $res->{'count'}++;
+    }
+
+    return $self->render(json => $res);
+}
+
+sub get {
+    my $self = shift;
+
+    my $id = $self->param('id');
+
+    my @fields = qw(id type name keywords add_date change_date metadata);
+    my $landmark = Rplus::Model::Landmark::Manager->get_objects(select => [@fields], query => [id => $id, delete_date => undef])->[0];
+    return $self->render_not_found unless $landmark;
+
+    return $self->render(json => {map { $_ => $landmark->$_ } @fields});
+}
+
+sub save {
+    my $self = shift;
+
+    my $id = $self->param('id') || undef;
+    my $type = $self->param('type') || undef;
+    my $name = trim(scalar $self->param('name')) || undef;
+    my $keywords = trim(scalar $self->param('keywords')) || undef;
+    my $geojson; eval { $geojson = decode_json(scalar($self->param('geojson'))); };
+    my $metadata = $self->param('metadata') || undef;
+
+    my $wkt;
+    if ($geojson) {
+        my $count = 0;
+        for my $f (@{$geojson->{'features'}}) {
+            next unless $f->{'geometry'}->{'type'} eq 'Polygon';
+            $wkt .= ',' if $count;
+            $wkt .= '('.join(',', map { '('.join(',', map { join(' ', grep { /^\d+(?:\.\d+)?$/ } @$_) } @$_).')' } @{$f->{'geometry'}->{'coordinates'}}).')';
+            $count++;
+        }
+    }
+
+    my $landmark;
+    if ($id) {
+        $landmark = Rplus::Model::Landmark::Manager->get_objects(query => [id => $id, delete_date => undef])->[0];
+        return $self->render(json => {status => 'failed'}) unless $landmark;
+    } else {
+        $landmark = Rplus::Model::Landmark->new;
+    }
+
+    $landmark->type($type);
+    $landmark->name($name);
+    $landmark->keywords($keywords);
+    if ($wkt) {
+        $landmark->geodata("postgis.ST_GeomFromEWKT('SRID=4326;MULTIPOLYGON($wkt)')");
+    } else {
+        $landmark->geodata(undef);
+    }
+    $landmark->metadata($metadata);
+
+    eval {
+        $landmark->save;
+    } or do {
+        return $self->render(json => {status => 'failed'});
+    };
+
+    return $self->render(json => {status => 'success', data => {id => $landmark->id}});
+}
+
+sub delete {
+    my $self = shift;
+
+    my $id = $self->param('id');
+
+    my $num_rows_updated = Rplus::Model::Landmark::Manager->update_objects(
+        set => {delete_date => \'now()'},
+        where => [id => $id, delete_date => undef],
+    );
+
+    return $self->render(json => {status => 'success'});
+}
+
+1;
