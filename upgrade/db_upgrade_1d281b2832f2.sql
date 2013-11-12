afdf36c72e9c899cf81177e6659094716748bf60
Alexander Orlenko <zxteam@gmail.com>
2013-11-12 16:55:18 +1100

    Added skip of hypnotoad.pid from vcs Added db upgrage sql based on commit hash

---
+++ upgrade/db_upgrade_1d281b2832f2.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.clients ALTER COLUMN name DROP NOT NULL;
+ALTER TABLE public.clients ALTER COLUMN login DROP NOT NULL;
+ALTER TABLE public.clients ADD CONSTRAINT clients_delete_date_chk CHECK (delete_date >= add_date);
+
+ALTER TABLE public.subscriptions ADD CONSTRAINT subscriptions_end_date_chk CHECK (end_date >= add_date);
+ALTER TABLE public.subscriptions ADD CONSTRAINT subscriptions_delete_date_chk CHECK (delete_date >= add_date);
