763f32884c7e4700fd098a09b96aa475c6b6bca2
raven <dai-katana@yandex.ru>
2014-01-08 18:14:06 +1100

    поправлен Config.pm, определяем путь к самому модулю, относительно него строим путь к файлу конфига.

--- lib/Rplus/Util/Config.pm
+++ lib/Rplus/Util/Config.pm
@@ -1,10 +1,17 @@
 package Rplus::Util::Config;
 
+use Cwd qw/abs_path/;
 use Mojo::Asset::File;
 
 sub get_config {
-    my $config_path = shift;
-    my $file = Mojo::Asset::File->new(path => $config_path);
+    # get path to Config.pm, then build a path to app.conf
+    my $module = __PACKAGE__;
+    $module =~s/::/\//g;
+    my $path = $INC{$module . '.pm'};
+    $path =~ s{^(.*/)[^/]*$}{$1};
+    $path = abs_path($path . '/../../../app.conf');
+    
+    my $file = Mojo::Asset::File->new(path => $path);
     my $config = eval $file->slurp;
     return $config;
 }
