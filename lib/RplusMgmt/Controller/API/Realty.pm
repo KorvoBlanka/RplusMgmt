fd3ec8b5e5cce492ed416f498b93c2364419d714
raven <dai-katana@yandex.ru>
2014-06-10 17:24:22 +1100

    fix

--- lib/RplusMgmt/Controller/API/Realty.pm
+++ lib/RplusMgmt/Controller/API/Realty.pm
@@ -39,26 +39,29 @@ my $_serialize = sub {
         # check owner phones for mediators
         my $mediator_str = undef;
         my $owner_phones = $realty->owner_phones;
-        my $mediator_iter = Rplus::Model::Mediator::Manager->get_objects_iterator(query => [phone_num => \@$owner_phones, delete_date => undef], require_objects => ['company']);
-        my $changed = 0;
-        if ($realty->agent_id == 10000) {
-            $realty->agent_id(undef);
-            $changed = 1;
-        }
-        while (my $mediator = $mediator_iter->next) {
-            $mediator_str = $mediator->company->name;
-            if($realty->agent_id != 10000) {
+
+        if(scalar @$owner_phones > 0) {
+            my $mediator_iter = Rplus::Model::Mediator::Manager->get_objects_iterator(query => [phone_num => \@$owner_phones, delete_date => undef], require_objects => ['company']);
+            my $changed = 0;
+            if ($realty->agent_id == 10000) {
+                $realty->agent_id(undef);
                 $changed = 1;
-                $realty->agent_id(10000);
-                if ($realty->state_code eq 'work') {
-                  $realty->state_code('raw');
+            }
+            while (my $mediator = $mediator_iter->next) {
+                $mediator_str = $mediator->company->name;
+                if($realty->agent_id != 10000) {
+                    $changed = 1;
+                    $realty->agent_id(10000);
+                    if ($realty->state_code eq 'work') {
+                      $realty->state_code('raw');
+                    }
                 }
             }
+            if($changed) {
+                $realty->save(changes_only => 1);
+            }
         }
-        if($changed) {
-            $realty->save(changes_only => 1);
-        }
-
+        
         my $x = {
             (map { $_ => ($_ =~ /_date$/ ? $self->format_datetime($realty->$_) : scalar($realty->$_)) } grep { !($_ ~~ [qw(delete_date geocoords landmarks metadata fts)]) } $realty->meta->column_names),
 
