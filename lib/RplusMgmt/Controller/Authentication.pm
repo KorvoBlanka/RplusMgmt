fc553efb74679659fb61b8881c78fa351e116630
raven <dai-katana@yandex.ru>
2014-06-16 13:33:46 +1100

    подключен биллинг

--- lib/RplusMgmt/Controller/Authentication.pm
+++ lib/RplusMgmt/Controller/Authentication.pm
@@ -10,13 +10,6 @@ my $ua = Mojo::UserAgent->new;
 sub auth {
     my $self = shift;
 
-    #my $tx = $ua->get('http://test.dvnic.com/api/account/get_by_domain?subdomain=account92');
-    #my $acc_data;
-    #if (my $res = $tx->success) {
-    #  $acc_data = $res->json;
-    #}
-
-    #return 1 if $acc_data->{blocked} == 0 && $acc_data->{balance} > 0 && $self->stash('user') && $self->stash('user')->{'id'};
     return 1 if $self->stash('user') && $self->stash('user')->{'id'};
 
     $self->render(template => 'authentication/signin');
@@ -30,19 +23,30 @@ sub signin {
     my $password = $self->param('password');
     my $remember_me = $self->param_b('remember_me');
 
-    return $self->render(json => {status => 'failed'}) unless $login && defined $password;
+    my $tx = $ua->get('http://rplusmgmt.com/api/account/get_by_domain?subdomain=' . $self->config->{'subdomain'});
+    my $acc_data;
+    if (my $res = $tx->success) {
+      $acc_data = $res->json;
+    }
+
+    return $self->render(json => {status => 'failed', reason => 'no_data'}) unless $login && defined $password;
 
     my $user = Rplus::Model::User::Manager->get_objects(query => [login => $login, password => $password, delete_date => undef])->[0];
-    return $self->render(json => {status => 'failed'}) unless $user;
+    return $self->render(json => {status => 'failed', reason => 'not_found'}) unless $user;
+
+    return $self->render(json => {status => 'failed', reason => 'not_found'}) unless $acc_data;
+
+    return $self->render(json => {status => 'failed', reason => 'no_money'}) unless $acc_data->{balance} > 0;
 
     $self->session->{'user'} = {
         id => $user->id,
         login => $user->login,
         role => $user->role,
+        mode => $acc_data->{mode},
     };
 
     if ($remember_me) {
-        $self->session(expiration => 604800);
+        $self->session(expiration => 36000);
     } else {
         $self->session(expiration => 3600); # default expiration
     }
